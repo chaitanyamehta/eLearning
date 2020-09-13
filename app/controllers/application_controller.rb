445fab884302a482460539ee660f8b248bed755f
Chaitanya Mehta <cmehta@ncsu.edu>
2020-09-12 23:24:24 -0400

    Add session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    if session[:user_auth_id]
+      @current_user ||= UserAuth.find(session[:user_auth_id]).authenticable
+    else
+      @current_user = nil
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:create]
+  get 'signup', to: 'students#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   resources :students
   resources :teachers
   resources :admins
   resources :courses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "courses#index"
 end
