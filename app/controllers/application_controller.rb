2f980cb9e6db3936fc5b21786d688446c2da1c71
Chaitanya Mehta <cmehta@ncsu.edu>
2020-09-13 16:48:59 -0400

    Add more helpers for login and authorrization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,16 @@ class ApplicationController < ActionController::Base
       @current_user_auth = nil
     end
   end
+
+  def current_user
+    unless current_user_auth.nil?
+      @current_user ||= current_user_auth.authenticable
+    else
+      @current_user = nil
+    end
+  end
+
+  def logged_in?       
+    !current_user_auth.nil?
+  end
 end
