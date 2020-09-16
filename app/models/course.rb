1662e10149999f9906397dd6d72586770bbe0439
Chaitanya Mehta <cmehta@ncsu.edu>
2020-09-16 01:03:58 -0400

    Add basic validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,6 +7,8 @@ class Course < ApplicationRecord
   validates :course_number, presence: true
   validates :name, presence: true
   validates :area, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validate :course_number_is_unique
 
   def mark_deleted
     update_attribute(:is_deleted, true)
@@ -14,5 +16,11 @@ class Course < ApplicationRecord
 
   def self.active_courses
     self.where(is_deleted: false)
+  end
+
+  def course_number_is_unique
+    if Course.find_by(course_number: course_number, is_deleted: false)
+      errors.add(:course_number, "has already been taken")
+    end
   end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -4,4 +4,6 @@ class Section < ApplicationRecord
   has_many :purchases
   has_many :feedbacks
   has_many :students, through: :purchases
+
+  validates :course_id, uniqueness: { scope: :teacher_id }
 end
