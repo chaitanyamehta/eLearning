b9fd41e46644a88b3a4bf8f61644783f0db3787e
Chaitanya Mehta <cmehta@ncsu.edu>
2020-09-16 20:52:57 -0400

    Restrict purchase of deleted course sections

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,4 +3,13 @@ class Purchase < ApplicationRecord
   belongs_to :section
   
   validates :section_id, uniqueness: { scope: :student_id }
+  validate :course_is_not_deleted
+
+  def course_is_not_deleted
+    if section.course.is_deleted
+      errors.add(:course, "has been deleted")
+    elsif section.teacher.is_deleted
+      errors.add(:course, "is not being taught by teacher")
+    end
+  end
 end
