6b5da3b6e855d919a5c149f08d7e28c59c538e0b
Chaitanya Mehta <cmehta@ncsu.edu>
2020-09-24 10:59:00 -0400

    Enhance coverage

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -47,6 +47,19 @@ RSpec.describe CoursesController do
     end
   end
 
+  describe 'show course' do
+    before :each do
+      @request.session[:user_auth_id] = FactoryBot.create(:admin).user_auth.id
+      @course = FactoryBot.create(:course)
+    end
+
+    it 'calls the model methods that gets a course' do
+      get :show, params: { id: @course.id }
+      expect(assigns(:course)).to eq(@course)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
   describe 'create course' do
     before :each do
       @request.session[:user_auth_id] = FactoryBot.create(:admin).user_auth.id
@@ -56,7 +69,32 @@ RSpec.describe CoursesController do
       course = FactoryBot.build(:course)
       course_params = { course_number: "#{course.course_number}", name: "#{course.name}", discipline_id: "#{course.discipline_id}", area: "#{course.area}", price: "#{course.price}" }
       expect(Course).to receive(:new).with(ActionController::Parameters.new(course_params).permit!).and_return(course)
-      post :create, :params => { :course => course_params }
+      post :create, params: { course: course_params }
+    end
+  end
+
+  describe 'edit course' do
+    before :each do
+      @request.session[:user_auth_id] = FactoryBot.create(:admin).user_auth.id
+    end
+
+    it 'status of course update is successful' do
+      course = FactoryBot.create(:course)
+      course_params = { course_number: "#{course.course_number}", name: "New Name", discipline_id: "#{course.discipline_id}", area: "#{course.area}", price: "#{course.price}" }
+      patch :update, params: { id: course.id, course: course_params }
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'delete course' do
+    before :each do
+      @request.session[:user_auth_id] = FactoryBot.create(:admin).user_auth.id
+    end
+
+    it 'destroy redirects to courses url' do
+      course = FactoryBot.create(:course)
+      delete :destroy, params: { id: course.id }
+      expect(response).to redirect_to(courses_url)
     end
   end
 end
