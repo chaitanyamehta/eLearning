641faef3837ec570dcdf8d45b81138b4fb1f2640
Chaitanya Mehta <cmehta@ncsu.edu>
2020-09-23 22:51:58 -0400

    Enhance test coverage for model Course

--- spec/factories/course.rb
+++ spec/factories/course.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :course do
     course_number { 'ECE 517' }
     name { 'Object Oriented Design' }
-    discipline { build(:discipline) }
+    discipline { Discipline.first || association(:discipline) }
     area { 'Software Engineering' }
     price { 100 }
     is_deleted { false }
